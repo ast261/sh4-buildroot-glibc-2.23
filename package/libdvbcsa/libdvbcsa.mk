d3b2dafc3ace1eaced86d51272c8b1fb4af6a3a4
ast261 <ast3795@gmail.com>
2025-06-23 20:27:25 +0200

    idl4k: adding newer libdvbcsa

--- package/libdvbcsa/libdvbcsa.mk
+++ package/libdvbcsa/libdvbcsa.mk
@@ -4,11 +4,17 @@
 #
 ################################################################################
 
-LIBDVBCSA_VERSION = 1.1.0
-LIBDVBCSA_SITE = http://get.videolan.org/libdvbcsa/$(LIBDVBCSA_VERSION)
+LIBDVBCSA_VERSION = 2e69227b2dba26d14589acdfc9a4667ec01a9383
+LIBDVBCSA_SITE = https://github.com/catalinii/libdvbcsa.git
+LIBDVBCSA_SITE_METHOD = git
 LIBDVBCSA_LICENSE = GPL-2.0+
 LIBDVBCSA_LICENSE_FILES = COPYING
-LIBDVBCSA_INSTALL_STAGING = YES
+
+LIBDVBCSA_PRE_CONFIGURE_HOOKS += LIBDVBCSA_RUN_BOOTSTRAP
+
+define LIBDVBCSA_RUN_BOOTSTRAP
+    cd $(@D) && ./bootstrap
+endef
 
 ifeq ($(BR2_X86_CPU_HAS_MMX),y)
 LIBDVBCSA_CONF_OPTS += --enable-mmx
