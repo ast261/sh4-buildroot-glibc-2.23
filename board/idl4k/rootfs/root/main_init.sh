1932979d2f64f6c3637278cecd23f0e978307e3f
ast261 <ast3795@gmail.com>
2025-07-09 20:25:53 +0200

    idl4k: add and initialize stm stapi

---
+++ board/idl4k/rootfs/root/main_init.sh
@@ -0,0 +1,8 @@
+#
+# this is script executed from /root/main_axe.out binary
+# after the AXE initialization routine
+# it is respawned immediately on exit
+#
+
+touch "/tmp/axe-done"
+while test 1; do sleep 999999999; done
